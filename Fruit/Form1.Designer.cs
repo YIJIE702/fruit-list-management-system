697fb379c4a93e2babaa78f4a2b4f70beed9cda6
YIJIE702 <tanyijie70@gmail.com>
2025-09-17 19:27:37 +0800

    Full code

---
+++ Fruit/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Fruit
+{
+    partial class frmFruit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtFruit = new System.Windows.Forms.TextBox();
+            this.lstFruit = new System.Windows.Forms.ListBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.userSelected = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(242, 23);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtFruit
+            // 
+            this.txtFruit.Location = new System.Drawing.Point(26, 23);
+            this.txtFruit.Name = "txtFruit";
+            this.txtFruit.Size = new System.Drawing.Size(199, 20);
+            this.txtFruit.TabIndex = 1;
+            // 
+            // lstFruit
+            // 
+            this.lstFruit.FormattingEnabled = true;
+            this.lstFruit.Location = new System.Drawing.Point(26, 63);
+            this.lstFruit.Name = "lstFruit";
+            this.lstFruit.Size = new System.Drawing.Size(199, 277);
+            this.lstFruit.TabIndex = 2;
+            this.lstFruit.SelectedIndexChanged += new System.EventHandler(this.lstFruit_SelectedIndexChanged);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(242, 100);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(242, 63);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(242, 145);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(75, 23);
+            this.btnClearAll.TabIndex = 5;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(242, 188);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // userSelected
+            // 
+            this.userSelected.AutoSize = true;
+            this.userSelected.Location = new System.Drawing.Point(242, 231);
+            this.userSelected.Name = "userSelected";
+            this.userSelected.Size = new System.Drawing.Size(81, 13);
+            this.userSelected.TabIndex = 7;
+            this.userSelected.Text = "<userSelected>";
+            this.userSelected.Visible = false;
+            // 
+            // frmFruit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 360);
+            this.Controls.Add(this.userSelected);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.lstFruit);
+            this.Controls.Add(this.txtFruit);
+            this.Controls.Add(this.btnAdd);
+            this.MinimizeBox = false;
+            this.Name = "frmFruit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fruit";
+            this.Load += new System.EventHandler(this.frmFruit_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtFruit;
+        private System.Windows.Forms.ListBox lstFruit;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label userSelected;
+    }
+}
+
---
+++ Fruit/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fruit
+{
+    public partial class frmFruit : Form
+    {
+        public frmFruit()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFruit_Load(object sender, EventArgs e)
+        {
+            LoadTextFile();
+        }
+
+        private void RefreshSorting()
+        {
+            List<string> termsList = new List<string>();
+
+            for (int i = 0; i <= lstFruit.Items.Count - 1; i++)
+            {
+                termsList.Add(lstFruit.Items[i].ToString());
+            }
+
+            string[] arr = termsList.ToArray();
+
+            Array.Sort(arr);
+
+            // reverse array 
+            //Array.Reverse(arr); 
+
+            lstFruit.Items.Clear();
+
+            foreach (string strFruit in arr)
+            {
+                lstFruit.Items.Add(strFruit);
+            } 
+
+        }
+
+        private void LoadTextFile()
+        {
+            string saveLocation = Application.StartupPath;
+            if (File.Exists(saveLocation + "\\test_file.txt"))
+            {
+                string inputLine = "";
+                int i = 0;
+                StreamReader myReader = new StreamReader(saveLocation + "\\test_file.txt", Encoding.Default);
+                inputLine = myReader.ReadLine();
+                lstFruit.Items.Clear();
+                while (inputLine != null)
+                {
+                    lstFruit.Items.Add(inputLine);           
+                    i++;
+                    inputLine = null;
+                    inputLine = myReader.ReadLine();
+                }
+                myReader.Close(); // Close the steamreader.
+                myReader.Dispose(); // Release all resources used by the System.Text.IO.TextReader object.    
+                RefreshSorting();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtFruit.Text.Trim() == "")
+            {
+                MessageBox.Show("Fruit is empty. Please check.", "Empty textbox", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtFruit.Focus();
+                return;
+            }
+            if (btnAdd.Text == "SAVE")
+            {
+                int selectedIndex = int.Parse(userSelected.Text);
+                selectedIndex -= 1;
+                if (selectedIndex <= 0)
+                {
+                    selectedIndex = 0;
+                }
+                lstFruit.Items.Insert(selectedIndex + 1, txtFruit.Text.Trim());
+                lstFruit.Items.RemoveAt(selectedIndex);
+                lstFruit.SelectedIndex = selectedIndex;
+                RefreshSorting();
+                Save();
+                txtFruit.Text = "";
+                btnAdd.Text = "Add";
+                btnDelete.Text = "Delete";
+                btnEdit.Enabled = true;
+                btnClearAll.Enabled = true;
+                btnSave.Enabled = true;
+                lstFruit.Enabled = true;
+            }
+            else // Mean Add Items
+            {
+                lstFruit.Items.Add(txtFruit.Text.Trim());    
+                RefreshSorting();
+                Save();
+                txtFruit.Text = "";
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lstFruit.SelectedIndex != -1)
+            {
+                txtFruit.Text = lstFruit.Items[lstFruit.SelectedIndex].ToString();
+                btnAdd.Text = "SAVE";
+                btnDelete.Text = "CANCEL";
+                txtFruit.Focus();
+                btnEdit.Enabled = false;
+                btnClearAll.Enabled = false;
+                btnSave.Enabled = false;
+                lstFruit.Enabled = false;
+            }         
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (btnDelete.Text == "CANCEL")
+            {
+                btnAdd.Text = "Add";
+                btnDelete.Text = "Delete";
+                txtFruit.Text = "";
+                txtFruit.Focus();
+                btnEdit.Enabled = true;
+                btnClearAll.Enabled = true;
+                btnSave.Enabled = true;
+                lstFruit.Enabled = true;
+            }
+            else
+            {
+                if (lstFruit.SelectedIndex != -1)
+                {
+                    lstFruit.Items.RemoveAt(lstFruit.SelectedIndex);
+                    Save();
+                }
+            }
+           
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            if (lstFruit.Items.Count == 0)
+            {
+                MessageBox.Show("There is no fruit list to be clear!", "No batch command", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (lstFruit.Items.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Remove all fruits from the list?", "Confirm Clear All", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    lstFruit.Items.Clear();
+                    txtFruit.Text = "";
+                    txtFruit.Focus();
+                    Save();
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void Save()
+        {
+            if (lstFruit.Items.Count == 0)
+            {
+                MessageBox.Show("There is no fruit found to be save.", "No data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                RefreshSorting();
+                string saveLocation = Application.StartupPath;
+                StreamWriter sw = new StreamWriter(saveLocation + "\\test_file.txt");
+                for (int i = 0; i <= lstFruit.Items.Count - 1; i++)
+                {
+                    sw.WriteLine(lstFruit.Items[i].ToString());
+                }
+                sw.Close();
+            }
+        }
+
+        private void lstFruit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int intCurrentSelection = lstFruit.SelectedIndex;
+            intCurrentSelection += 1;
+            userSelected.Text = intCurrentSelection.ToString();
+        }
+
+    }
+}
